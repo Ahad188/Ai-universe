4f3ad6996d34e54c0f109f1aaeb57cf0dd87a24f
AhaD <abuahad35188@gmail.com>
2023-03-03 19:02:40 +0600

    recheck all code

--- js/app.js
+++ js/app.js
@@ -59,9 +59,9 @@ const loadApi = (dataLimit) =>{
           const body = document.getElementById('modal-body');
           body.innerHTML = " ";
           body.innerHTML +=`
-          <div class="contaoner">
-               <div class="row">
-                    <div class="col-10 col-md-6 mx-auto">
+          <div class="contaoner bg-light bg-gradient p-3">
+               <div class="row g-4">
+                    <div class="col-10 col-md-6 mx-auto border">
                     <div class="card-body">
                     <p class="card-text"> <p class="card-text text-center fw-bold"> ${data.description}</p></p>
                     <div class="d-flex align-items-around mx-auto">
@@ -78,33 +78,32 @@ const loadApi = (dataLimit) =>{
                          <span class="p-2 text-center text-info-emphasis">${data.pricing[2].price}</span>
                     </div>
                  </div>
-                 <div class="d-flex">
+               <div class="d-flex">
               <ul class="text-mute">
-              <h5>Features</h5> <br/>
+                        <h5>Features</h5> <br/>
                         <li>${data.features[1].feature_name}</li>
                         <li>${data.features[2].feature_name}</li>
                         <li>${data.features[3].feature_name}</li>  
                </ul>
                    <ul class = "text-muted">
-                   <h5>Intergations</h5> <br/>
+                        <h5>Intergations</h5> <br/>
                         <li>${data.integrations[0]}</li>
                         <li>${data.integrations[1]}</li>
                         <li>${data.integrations[2]}</li> 
                    </ul>
                          </div>
-                              </div>
+                         </div>
                     </div>
-                    <div class="col-10 col-md-6 mx-auto">
+                    <div class="col-10 col-md-6 mx-auto border">
                     <div class="card mt-3" style="width: 28rem;">
                          <img src="${data.image_link[0]}" class="card-img-top" alt="...">
                          <div class="card-body">
-                         <p class="card-text">${data.input_output_examples ? data.input_output_examples[0].input : 'No! Not Yet! Take break!!!'}</p>
-                         <p class="card-text">${data.input_output_examples ? data.input_output_examples[0].output : 'No! Not Yet! Take break!!!'}</p>
+                         <p class="card-text text-center">${data.input_output_examples ? data.input_output_examples[0].input : 'No! Not Yet! Take break!!!'}</p>
+                         <p class="card-text text-center">${data.input_output_examples ? data.input_output_examples[0].output : 'No! Not Yet! Take break!!!'}</p>
                          
                          </div>
                          <div class="accuracy">${(data.accuracy.score !== null) ? `<button>${Math.floor(data.accuracy.score * 100)}% accracy</button>` : " " }</div>
                     </div>
-                    
                     </div>
                </div>
           </div>
